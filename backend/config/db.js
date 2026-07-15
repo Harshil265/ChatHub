9fd4ae4b8a5a17118a9198ca1758d55668d3ae59
Harshil <harshilkoladiya423@gmail.com>
2026-07-14 23:22:06 -0700

    ..

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,18 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
+        await mongoose.connect(process.env.MONGO_URI);
 
-        await mongoose.connect("mongodb://127.0.0.1:27017/chathub");
-
-        console.log("✅ MongoDB Connected");
-
+        console.log("✅ MongoDB Atlas Connected");
     } catch (error) {
-
         console.error("❌ Database Connection Failed");
-        console.error(error.message);
-
+        console.error(error);
         process.exit(1);
-
     }
 };
 
