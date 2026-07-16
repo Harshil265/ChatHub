9d206e494f891892ff17b1627c9be8b883679d53
Koladiya Harshil <harshilkoladiya423@gmail.com>
2026-07-16 09:42:35 -0700

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -30,7 +30,8 @@ async function login() {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch(" https://chat-app-uiip.onrender.com
+/api/auth/login", {
 
             method: "POST",
 
