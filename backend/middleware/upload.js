97ebc0cc937360bb473511f5dadd160ed43d3619
Koladiya Harshil <harshilkoladiya423@gmail.com>
2026-07-17 09:17:06 -0700

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,53 +1,24 @@
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-// Create folder automatically
-const uploadPath = path.join(__dirname, "../uploads/profile");
-
-if (!fs.existsSync(uploadPath)) {
-
-    fs.mkdirSync(uploadPath, { recursive: true });
-
-}
-
-const storage = multer.diskStorage({
-
-    destination: (req, file, cb) => {
-
-        cb(null, uploadPath);
-
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "ChatHub_Profile",
+        allowed_formats: ["jpg", "jpeg", "png", "webp"],
     },
-
-    filename: (req, file, cb) => {
-
-        const uniqueName =
-            Date.now() + path.extname(file.originalname);
-
-        cb(null, uniqueName);
-
-    }
-
 });
 
 const fileFilter = (req, file, cb) => {
-
     if (file.mimetype.startsWith("image/")) {
-
         cb(null, true);
-
     } else {
-
         cb(new Error("Only images are allowed"));
-
     }
-
 };
 
 module.exports = multer({
-
     storage,
-
-    fileFilter
-
+    fileFilter,
 });
