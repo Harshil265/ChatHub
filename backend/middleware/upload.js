c9241c033ebf2aba0d687afba128a93619b49913
Koladiya Harshil <harshilkoladiya423@gmail.com>
2026-07-18 09:36:30 -0700

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,65 +1,18 @@
 const multer = require("multer");
-const { CloudinaryStorage } =
-    require("multer-storage-cloudinary");
 
-const cloudinary =
-    require("../config/cloudinary");
-
-const storage = new CloudinaryStorage({
-
-    cloudinary: cloudinary,
-
-    params: {
-
-        folder: "ChatHub_Profile",
-
-        allowed_formats: [
-            "jpg",
-            "jpeg",
-            "png",
-            "webp"
-        ]
-
-    }
-
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
 
-    console.log(
-        "FILE RECEIVED:",
-        file.originalname
-    );
-
-    console.log(
-        "FILE TYPE:",
-        file.mimetype
-    );
-
-    if (
-        file.mimetype.startsWith("image/")
-    ) {
-
+    if (file.mimetype.startsWith("image/")) {
         cb(null, true);
-
-    }
-
-    else {
-
-        cb(
-            new Error(
-                "Only images are allowed"
-            )
-        );
-
+    } else {
+        cb(new Error("Only images are allowed"));
     }
 
 };
 
 module.exports = multer({
-
     storage,
-
     fileFilter
-
 });
