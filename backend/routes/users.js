67690b86d7be5b6c8294faac48db1f8bf734c819
Koladiya Harshil <harshilkoladiya423@gmail.com>
2026-07-18 00:39:40 -0700

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,31 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/authMiddleware");
-
 const upload = require("../middleware/upload");
-const { uploadProfilePicture } = require("../controllers/userController");
 
 const {
     getAllUsers,
     getCurrentUser,
-    updateProfile
+    updateProfile,
+    uploadProfilePicture
 } = require("../controllers/userController");
 
-// Get all users
 router.get("/", auth, getAllUsers);
 
-// Get logged-in user
 router.get("/me", auth, getCurrentUser);
 
-// Update profile
-router.put("/update-profile", auth, updateProfile);
+router.put(
+    "/update-profile",
+    auth,
+    updateProfile
+);
 
 router.post(
     "/upload-profile",
-    (req, res, next) => {
-        console.log("🔥 UPLOAD ROUTE HIT");
-        next();
-    },
     auth,
     upload.single("profilePic"),
     uploadProfilePicture
